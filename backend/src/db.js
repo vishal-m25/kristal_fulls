d3e75fc4b151cb17cdfd1d0ca4a988e2f975f0dc
vishal-m25 <vellimuthu9@gmail.com>
2025-08-22 13:52:24 +0530

    env access updation

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,10 +1,9 @@
 // src/db.js
 import mongoose from "mongoose";
 
-
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
